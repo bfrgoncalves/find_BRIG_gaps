d2f702df9877a63e41d5e4e0e8aab8aa0b45801b
bfrgoncalves <brunofiliperg@gmail.com>
2015-08-06 17:03:49 +0100

    version 1.3

---
+++ BLAST_to_BRIG.py
@@ -0,0 +1,55 @@
+import subprocess
+import argparse
+import os
+import shutil
+from os import listdir
+from os.path import isfile, join, isdir
+import sys
+from datetime import datetime
+import numpy
+import string
+import operator
+import collections
+import csv
+import HTSeq
+
+from Bio.Blast import NCBIXML
+
+
+def main():
+
+	parser = argparse.ArgumentParser(description="This program parses a .xml BLAST results file into a .tab BRIG format file")
+	parser.add_argument('-x', nargs='?', type=str, help=".xml BLAST results file", required=True)
+	parser.add_argument('-o', nargs='?', type=str, help="results file name", required=True)
+
+
+	args = parser.parse_args()
+
+
+def BLAST_to_BRIG(BLASTfile, resultsFile):
+
+	rec = open(bOutFile)
+	blast_records = NCBIXML.parse(rec)
+
+	with open(resultsFile, 'w') as tabFile: 
+	
+		for blast_record in blast_records:
+			bestmatchArray[str(blast_record.query)] = []
+			
+			for alignment in blast_record.alignments:
+				for match in alignment.hsps:
+					tabFile.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % 
+								str(blast_record.query),
+								str(alignment.hit_def),
+								str(match.identities),
+								str(alignment.length),
+								str(match.query_start),
+								str(match.query_start + alignment.length),
+								str(match.sbjct_start),
+								str(match.query_start + alignment.length))
+
+					break
+
+
+if __name__ == "__main__":
+	main()
